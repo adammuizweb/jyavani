a390eed2fd95bbb20c9e5805cc980bd3f427ae4a
Adam Muiz <az@adammuiz.com>
2026-05-26 14:38:31 +0700

    chore: remove temp check file

--- tmp_check.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=jyavani','root','muiz');
-$st = $pdo->query("SELECT `key`, `value` FROM settings WHERE `key` IN ('sidebar_enabled','sidebar_controller_overrides')");
-foreach ($st as $r) {
-    echo $r['key'] . '=' . $r['value'] . PHP_EOL;
-}
